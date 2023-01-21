06189107306f22dd3b1e76764c289e6fbacc5403
Ogunnaike Ibukunoluwa <ibukunoluwamayowa5@gmail.com>
2023-01-21 20:55:05 +0100

    personal website

---
+++ index.js
@@ -0,0 +1,11 @@
+//space, scary, romantic, cowboy, fantacy, military, superhero
+favoriteMovieGenre("space");
+
+//watermelon, pineapple, banana, blueberry, avocado
+favoriteFruit("banana");
+
+//light, dark
+favoriteTheme("dark");
+
+//sharp, soft, round
+favoriteEdgeStyle("round");
